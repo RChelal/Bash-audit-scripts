133a2910358242837bb720636680f3b9aa71b2ee
RChelal <rodneychelal@gmail.com>
2024-05-16 08:00:56 +0300

    declare sonaqube function

--- sonarqube.sh
+++ sonarqube.sh
@@ -8,10 +8,11 @@ project="project1"
 token="sqp_b8de888token1"
 
 runscanner() {
-    echo "Initiating $project source code audit"
-    cd "$path$project"
-    "$sonarscanner" "$projectvar$project" "$scanner$token"
-    echo "Completed scanning $project source code audit. Proceed to SonarQube portal for source code review."
+    echo "Initiating $project source code audit";
+    cd $path$project;
+    $sonarscanner $projectvar$project $scanner$token;
+    echo "Completed scanning $project source code audit. Proceed to SonarQube portal for source code review.";
+    sleep 3
 }
       echo "Confirming running images"
       sudo docker ps | grep $docker
@@ -19,11 +20,10 @@ if docker ps | grep -q $docker; then
       # Run the sonar-scanner commands
       # run first source code
       runscanner
-      # create variable for project channel request
+      # create variable for project channel request run seconf source code function
 	project="project2"
 	token="sqp_b8de88token2"
-      # run 2nd source code audit
-      runscanner
+ 	runscanner
       # run 3rd source code audit  
 
       echo "Thank you for using sonarqube scanning tool adios. Proceed to the sonarqube portal to get the results of the report"
